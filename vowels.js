66f5d7261ed1bc806b124c72d0569a0e69e8f106
MillieNakiganda <millienakiganda@gmail.com>
2019-08-29 22:54:10 +0300

    vowels challenge

---
+++ vowels.js
@@ -0,0 +1,36 @@
+let vowels = ['a','i','e','o','u'];
+let characters;
+let counter = 0;
+let result = [];
+let occurrences = [];
+
+
+function returnvowels(names){
+
+
+     for(let i = 0;i<names.length; i++)
+       {
+        if( names.indexOf(names[i],i+1) != -1)
+          {
+           counter++;
+          }
+         for(let j = 0; j<vowels.length; j++)
+           {
+            if(names[i] === vowels[j])
+             {
+                characters = result.find(item => item == names[i]);
+            if(characters === undefined)
+              {
+                result.push(names[i]);
+              }
+          
+            }
+         }
+     }
+
+occurrences.push(counter);
+//alert([].concat(result.join(""),occurrences));
+return ([].concat(result.join(""),occurrences));
+}
+
+alert (returnvowels("dahdah"));
