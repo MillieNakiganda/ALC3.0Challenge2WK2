80a8b5e74c245d64e32b54c669a50317aef58e95
MillieNakiganda <millienakiganda@gmail.com>
2019-08-29 23:02:43 +0300

    adding fizzbuzz challenge

---
+++ fizzbuzz/fizzbuzz.js
@@ -0,0 +1,22 @@
+function fizzBuzz(list1, list2) {
+    let sum = list1.length + list2.length;
+
+    if(sum % 3 == 0 && sum % 5 == 0 )
+    {
+       return "Fizzbuzz";
+    }
+    else if(sum % 3 == 0)
+    {
+        return "Fizz";
+    }
+    else if(sum % 5 == 0)
+    {
+        return "Buzz";
+    }
+    else if(sum % 3 != 0 && sum % 5 != 0)
+    {
+       return sum;
+    }
+}
+
+module.exports = fizzBuzz;
